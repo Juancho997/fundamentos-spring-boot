95de749cf9604704d4440825824538c6e2466b6b
Juan Bonino <tomasboni9@gmail.com>
2022-10-13 20:37:59 -0300

    feature: added DB connection > H2 - an in-memory persistency database

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanWithPropertiesImplement
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 // habilitamos la clase UserPojo para ser inyectada como dependencia♦
@@ -24,5 +27,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplement(name, lastname);
+    }
+
+    //SpringBoot inyectará esta dependencia para así poder conectar con la BD
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
