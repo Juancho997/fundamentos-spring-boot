e3ced6bd9dd6a44a107480a8b12ff874d0110ec9
Juan Bonino <tomasboni9@gmail.com>
2022-10-13 17:18:53 -0300

    feature: userPojo

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,7 +2,9 @@ package com.fundamentos.springboot.fundamentos;
 
 import com.fundamentos.springboot.fundamentos.bean.MyBean;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,19 +13,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	// "Implementación" es la clase que representa a un bean !!!
+
 	// me traigo la dependencia
 	private final ComponentDependency componentDependency;
 	private final MyBean myBean;
 	private final MyBeanWithDependency myBeanWithDependency;
+	private final MyBeanWithProperties myBeanWithProperties;
+	private final UserPojo userPojo;
+
 
 	// paso al constructor la dependencia a inyectar
 	// se puede tener n implementaciones a partir de una dependencia
 	// utilizando @Qualifier indicamos la dependencia a inyectar
 	// en este caso, debido a que tenemos dos dependencias que implementan la misma interfaz, elegimos "componentTwoImplement"
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -35,5 +44,7 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword() + "-" + userPojo.getAge());
 	}
 }
